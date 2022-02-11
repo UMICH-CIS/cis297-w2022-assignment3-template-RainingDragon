889ac8b88d626235f352d99741bc90f1543f61fa
Charles <cesimms@umich.edu>
2022-02-10 22:50:17 -0500

    added Account class

--- BankApplication/Program.cs
+++ BankApplication/Program.cs
@@ -8,6 +8,45 @@ namespace BankApplication
 {
     class Program
     {
+        public class Account
+        {
+            private decimal balance;
+            public decimal Balance
+            {
+                get { return balance; }
+                set
+                {
+                    if (value < 0)
+                    {
+                        balance = 0;
+                        Console.WriteLine("Beginning balance was incorrect");
+                    }
+                    else
+                        balance = value;
+                }
+            }
+            public Account(decimal y)
+            {
+                Balance = y;
+            }
+            public virtual void Credit(decimal x)
+            {
+                balance += x;
+            }
+            public virtual bool Debit(decimal x)
+            {
+                if (balance < x)
+                {
+                    Console.WriteLine("Debit amount exceeded account balance.");
+                    return false;
+                }
+                else
+                {
+                    balance -= x;
+                    return true;
+                }
+            }
+        }
         static void Main(string[] args)
         {
         }
